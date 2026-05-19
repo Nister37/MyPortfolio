c6c41bc4c0bc608e24658a538c9ef8762a132ee0
Paweł Ryfiak <pawel.ryfiak@student.kdg.be>
2026-05-19 22:54:38 +0200

    style: configure tailwind daisyui and sass

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,12 @@ import svelte from '@astrojs/svelte';
 
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()]
+  integrations: [svelte(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
